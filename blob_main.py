66354e77cd977f623866d4c0f328794f6e598bc4
Ria Cordero <riaacordero@Rias-MacBook-Pro.local>
2021-01-25 17:46:15 +0800

    Initial commit

---
+++ blob_main.py
@@ -0,0 +1,315 @@
+import pygame
+from pygame import display
+from pygame.locals import *
+
+pygame.init()
+clock = pygame.time.Clock()
+fps = 60
+
+# SET SCREEN
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Blob!")
+
+# FONT
+font_title = pygame.font.SysFont('Bahaus 93',30)
+white = (255,255,255)
+
+# GRID VARIABLES
+tile_size = 25
+game_over = 0
+main_menu = True
+
+# LOAD IMAGES
+bg_img = pygame.image.load('img/bg_img.png')
+restart_img = pygame.image.load('img/restart_button.png')
+start_img = pygame.image.load('img/start_button.png')
+exit_img = pygame.image.load('img/exit.png')
+win_img = pygame.image.load('img/win_bg_img.png')
+start_screen_img = pygame.image.load('img/start.png')
+lose_screen_img = pygame.image.load('img/lose.png')
+
+def display_txt(text, font, text_color, x,y):
+    img = font.render(text, True, text_color)
+    screen.blit(img,(x,y))
+
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+
+    def draw(self):
+        mouse_act = False
+
+        # get mouse position
+        mouse_pos = pygame.mouse.get_pos()
+
+        # mouse over
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:      # 0 is for left-click
+                mouse_act = True
+                self.clicked = True
+
+        # mouse click
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # draw button
+        screen.blit(self.image, self.rect)
+
+        return mouse_act
+
+class Player():
+    def __init__(self, x,y):
+        self.reset(x,y)
+    
+    def update(self, game_over): 
+        d_x = 0
+        d_y = 0
+
+        if game_over == 0:
+            # KEY PRESS CONTROLS
+            keypress = pygame.key.get_pressed()
+            if keypress[pygame.K_SPACE] and self.jumped == False and self.mid_air == False:
+                self.v_y = -15
+                self.jumped = True
+            if keypress[pygame.K_SPACE] == False:
+                self.jumped = False
+            if keypress[pygame.K_LEFT]:
+                d_x -= 5
+            if keypress[pygame.K_RIGHT]:
+                d_x += 5
+
+            # GRAVITY
+            self.v_y += 1
+            if self.v_y > 10:
+                self.v_y = 10
+            d_y += self.v_y
+
+            # COLLISION
+            self.mid_air = True
+            for tile in world.tile_list:
+                # X_direction
+                if tile[1].colliderect(self.rect.x+d_x,self.rect.y, self.width,self.height):
+                    d_x =0
+                # Y-direction
+                if tile[1].colliderect(self.rect.x,self.rect.y +d_y, self.width,self.height):
+                    # JUMP
+                    if self.v_y < 0:
+                        d_y = tile[1].bottom - self.rect.top
+                        self.v_y = 0
+                    # FALL
+                    elif self.v_y >= 0:
+                        d_y = tile[1].top - self.rect.bottom
+                        self.mid_air = False
+            
+            # ENEMY COLLISION
+            if pygame.sprite.spritecollide(self, enemy_grp, False):
+                game_over = -1
+
+            # LAVA COLLISION
+            if pygame.sprite.spritecollide(self, lava_grp, False):
+                game_over = -1
+
+            # DOOR COLLISION
+            if pygame.sprite.spritecollide(self, door_grp, False):
+                game_over = 1
+
+            # COORD UPDATES
+            self.rect.x += d_x
+            self.rect.y += d_y
+
+        elif game_over == -1:
+            self.image = self.dead_img
+
+		# To draw player into game 
+        screen.blit(self.image, self.rect)
+        return game_over
+
+    def reset(self,x,y):
+        # ALIVE
+        img = pygame.image.load('img/blob.png')
+        self.image = pygame.transform.scale(img, (30,30))
+        self.rect = self.image.get_rect()
+
+        # DEAD
+        dead_img = pygame.image.load('img/dead.png')
+        self.dead_img = pygame.transform.scale(dead_img, (30,30))
+        self.rect = self.image.get_rect()
+
+        # POSITION
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.v_y = 0
+        self.jumped = False
+        self.direc = 0
+        self.mid_air = True
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+ 
+        #img
+        floor = pygame.image.load('img/ground.png')
+
+        row_count = 0
+        for row in data:
+            column_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(floor, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = column_count*tile_size
+                    img_rect.y = row_count*tile_size
+                    tile = (img,img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    fire = Enemy(column_count*tile_size, row_count*tile_size - 30)
+                    enemy_grp.add(fire)
+                if tile == 3:
+                    lava = Lava(column_count*tile_size, row_count*tile_size)
+                    lava_grp.add(lava)
+                if tile == 4:
+                    door = Door(column_count*tile_size, row_count*tile_size - (tile_size // 2))
+                    door_grp.add(door)
+                column_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0],tile[1])
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x,y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/fireball.png')
+        self.image = pygame.transform.scale(img, (30,30))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_count = 0
+
+    def update(self):
+        self.rect.y += self.move_direction
+        self.move_count += 1
+        if self.move_count > 20:
+            self.move_direction *= -1
+            self.move_count *= -1
+
+class Lava(pygame.sprite.Sprite):
+    def __init__(self, x,y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/lava.png')
+        self.image = pygame.transform.scale(img, (tile_size,tile_size))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Door(pygame.sprite.Sprite):
+    def __init__(self, x,y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/door.png')
+        self.image = pygame.transform.scale(img, (tile_size,int(tile_size*1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+# WORLD DATA
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1],  
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 1, 3 , 3, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1 , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# CREATE GAME MOBS
+player = Player(100, screen_height -130)
+enemy_grp = pygame.sprite.Group()
+lava_grp = pygame.sprite.Group()
+door_grp = pygame.sprite.Group()
+world = World(world_data)
+
+# CREATE BUTTONS
+restart_btn = Button(screen_width // 2 - 90, screen_height // 2, restart_img)
+start_btn = Button(screen_width // 2 - 75, screen_height // 2, start_img)
+exit_btn = Button(screen_width // 2 + 10, screen_height // 2, exit_img)
+
+# GAME LOOP
+Running = True
+
+while Running:
+
+    clock.tick(fps)
+    screen.blit(bg_img, (0,0))
+
+    if main_menu == True:
+        screen.blit(start_screen_img, (0,0))
+        if exit_btn.draw():
+            Running = False
+        if start_btn.draw():
+            main_menu = False
+    
+    else:
+        world.draw()
+
+        if game_over == 0:
+            enemy_grp.update()
+            
+        enemy_grp.draw(screen)
+        lava_grp.draw(screen)
+        door_grp.draw(screen)
+
+        game_over = player.update(game_over)
+
+        # LOSE
+        if game_over == -1:
+            screen.blit(lose_screen_img, (0,0))
+            if restart_btn.draw():
+                player.reset(100, screen_height -130)
+                game_over = 0
+            if exit_btn.draw():
+                main_menu = True
+                player.reset(100, screen_height -130)
+                game_over = 0
+        
+        # WIN
+        if game_over == 1:
+            screen.blit(win_img, (0,0))
+            if restart_btn.draw():
+                player.reset(100, screen_height -130)
+                game_over = 0 
+            if exit_btn.draw():
+                main_menu = True
+                player.reset(100, screen_height -130)
+                game_over = 0
+    
+    for event in pygame.event.get():
+        if event.type ==  pygame.QUIT:
+            Running = False
+
+    pygame.display.update()
+pygame.quit()
