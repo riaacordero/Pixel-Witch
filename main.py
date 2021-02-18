28a87007cb6c264fa810b9b5e7fd67d372ec5b53
Hernan Rey Jugar <47274035+hrjugar@users.noreply.github.com>
2021-02-17 22:29:01 -0800

    Editing of score display in level and level selection screens (#15)
    * Moved enum classes to a different module
    
    * Transferred font, img, and sound folder to assets package
    
    * Revert "Transferred font, img, and sound folder to assets package"
    
    This reverts commit ed658631
    
    * Transferred font, img, and sound folder to assets package
    
    * Split the code into different modules for organization
    
    * Minor cleanups and added documentation
    
    * Minor cleanups and added documentation
    
    * Minor cleanups and added documentation
    
    * Removed print statements
    
    * Made gems replace enemies when killed
    
    * message
    
    * Improved fireball mechanism
    
    * Implemented shield mechanism, fixed fireball sfx bugs, and cleaned up.
    
    * Added retry option in pause menu
    
    * Use of Player.reset() transferred to Level.reset()
    
    * Applied numpy in initializing level array
    
    * Fixed bug where shield blink would stay after level reset
    
    * Shield bug fixes
    
    * Added placeholder sfx for both shield activation and blinking
    
    * Added select sfx for other buttons
    
    * Changed "retry" word usage to "restart"
    
    * Added level selection placeholders and level dictionary
    
    * changed "main menu" to "start screen", "level selection" to "main menu", and implemented level selection
    
    * updated terminology for variables
    
    * Implemented "next" functionality of game clear
    
    * Changed placement of sfx .play() usages
    
    * Reverted back "main menu" and "level select" terminologies
    
    * Updated other variables in reverting terminologies
    
    * removed some comments and print() uses
    
    * Added title and how-to text in main menu
    
    * Added placeholder levels three to five
    
    * Changed level dicts to level list
    
    * Added levels in Location enum
    
    * Added and implemented lava platforms
    
    * Changed score display speed
    
    * Transferred how-to folder to img folder
    
    * Implemented the how-to menu
    
    * Added flexibility for level background image and music
    
    * Added and implemented overlay and torch mechanism
    
    * Cleaned up Level class
    
    * Made torch and overlay mechanism exclusive for underground levels
    
    * Adjusted overlay size to fix bug
    
    * Cleaned up levels code
    
    * Implemented high scores in levels
    
    * Highlights high score in level selection if max score is reached
    
    * Edited max gems attainable in level 5 and 8
    
    * Changed score in level to green if max score is reached
    
    * Resized and repositioned score in level selection screen
    
    Co-authored-by: Ria Cordero <41848305+riaacordero@users.noreply.github.com>

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ for i in range(len(levels_data)):
                                                pos="center"))
 selection_high_score_texts = []
 for i in range(len(levels_data)):
-    selection_high_score_texts.append(Text(i % 3 * 125 + 125, 180 if i < 3 else 305 if i < 6 else 430,
-                                           str(0), fff_forward_font, 20, gray, pos="center"))
+    selection_high_score_texts.append(Text(i % 3 * 125 + 125, 185 if i < 3 else 310 if i < 6 else 435,
+                                           str(0), fff_forward_font, 16, gray, pos="center"))
 
 over_text = Text(125, 300, "GAME OVER", fff_forward_font, 32, black)
 over_restart_text = HoverableText(190, 385, "restart", retro_gaming_font, 24, dark_gray, light_gray, gray)
@@ -158,6 +158,7 @@ def display_level_select():
         current_location = Location.MAIN_MENU
     for level in levels:
         if level.button.is_clicked():
+            score_text.update(new_text="0", new_color=purple)
             music_player.stop_and_unload()
             select_sfx.play()
             level.reset()
@@ -179,6 +180,7 @@ def display_pause(level: Level):
         paused = False
 
     if pause_restart_text.is_clicked():
+        score_text.update(new_color=purple)
         music_player.stop_and_unload()
         level.reset()
         current_player_state = player.player_state
@@ -200,6 +202,7 @@ def display_game_over(level: Level):
         select_sfx.play()
 
     if over_restart_text.is_clicked():
+        score_text.update(new_text="0", new_color=purple)
         level.reset()
         current_player_state = player.player_state
     elif over_main_text.is_clicked():
@@ -236,10 +239,12 @@ def display_game_clear(level: Level):
             from_start_or_main = False
             current_location = Location.MAIN_MENU
         else:
+            score_text.update(new_text="0", new_color=purple)
             levels[level.number].reset()
             current_player_state = player.player_state
             current_location += 1
     elif clear_restart_text.is_clicked():
+        score_text.update(new_text="0", new_color=purple)
         level.reset()
         current_player_state = player.player_state
     elif clear_main_text.is_clicked():
@@ -258,7 +263,7 @@ def display_level(level: Level):
         screen.blit(key_img if player.has_key else key_inactive, (10, 10))
         screen.blit(gem_img, (55, 10))
         score_text.draw(screen)
-        score_text.update(str(level.score))
+        score_text.update(str(level.score), new_color=light_green if level.score >= level.max_score else ())
 
     pause_btn.update()
     pause_btn.draw(screen)
@@ -278,7 +283,7 @@ def display_level(level: Level):
             index = level.number - 1
             selection_high_score_texts[index].update(str(level.high_score), pos="center",
                                                                 new_x=index % 3 * 125 + 125,
-                                                                new_y=180 if index < 3 else 305 if index < 6 else 430,
+                                                                new_y=185 if index < 3 else 310 if index < 6 else 435,
                                                                 new_color=green if level.high_score >= level.max_score
                                                                 else ())
             selection_high_score_texts[index].update(new_color=green)
--- sound.py
+++ sound.py
@@ -28,6 +28,7 @@ player_atk_sfx = pygame.mixer.Sound(r"assets/sound/attack.wav")
 enemy_hit_sfx = pygame.mixer.Sound(r"assets/sound/hit.wav")
 shield_sfx = pygame.mixer.Sound(r"assets/sound/shield.wav")
 shield_blink_sfx = pygame.mixer.Sound(r"assets/sound/select1.wav")
+acquired_max_score_sfx = pygame.mixer.Sound(r"assets/sound/win.wav")
 
 
 class MusicPlayer:
